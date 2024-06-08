22ce33bf5a5db5e58ee2d8ecd0e29180dc685df9
Chris Alfano <chris@jarv.us>
2024-06-08 15:48:27 -0400

    test authenticating and selecting data

--- api-test/index.js
+++ api-test/index.js
@@ -3,8 +3,29 @@ import { createClient } from '@supabase/supabase-js'
 // Create a single supabase client for interacting with your database
 const supabase = createClient('http://localhost:8000', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyAgCiAgICAicm9sZSI6ICJhbm9uIiwKICAgICJpc3MiOiAic3VwYWJhc2UtZGVtbyIsCiAgICAiaWF0IjogMTY0MTc2OTIwMCwKICAgICJleHAiOiAxNzk5NTM1NjAwCn0.dc_X5iR_VP_qT0zsiyj_I_OZ2T9FtRU2BBNWN8Bu4GE')
 
-// Sign in with phone number
-const { data, error } = await supabase.auth.signInWithOtp({
-    phone: '+12679186166',
-})
+const phone = ''
 
+// Sign in with phone number
+// const { data, error } = await supabase.auth.signInWithOtp({
+//     phone,
+// })
+
+// Verify the OTP
+// const {
+//     data: { session },
+//     error,
+// } = await supabase.auth.verifyOtp({
+//     phone,
+//     token: '368908',
+//     type: 'sms',
+// })
+
+const accessToken = ''
+const refreshToken = ''
+
+
+const { data, error } = await supabase
+  .from('topics')
+  .select()
+
+debugger
