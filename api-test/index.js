70bea896ded3b641a74b6617f7e7276cebc7beae
Chris Alfano <chris@jarv.us>
2024-06-08 18:31:17 -0400

    fix api-test auth flow

--- api-test/index.js
+++ api-test/index.js
@@ -7,29 +7,38 @@ dotenvConfig('../.env')
 // Create a single supabase client for interacting with your database
 const supabase = createClient(process.env.SUPABASE_URL, process.env.SUPABASE_ANON_KEY)
 
-const phone = process.env.SUPABASE_USER_PHONE
 
-// Sign in with phone number
-// const { data, error } = await supabase.auth.signInWithOtp({
-//     phone,
-// })
+async function run() {
+  const phone = process.env.SUPABASE_USER_PHONE
 
-// Verify the OTP
-// const {
-//     data: { session },
-//     error,
-// } = await supabase.auth.verifyOtp({
-//     phone,
-//     token: '368908',
-//     type: 'sms',
-// })
+  // Sign in with phone number
+  // const { data, error } = await supabase.auth.signInWithOtp({
+  //     phone,
+  // })
 
-const accessToken = process.env.SUPABASE_USER_ACCESS_TOKEN
-const refreshToken = process.env.SUPABASE_USER_REFRESH_TOKEN
+  // Verify the OTP
+  // const {
+  //     data: { session },
+  //     error,
+  // } = await supabase.auth.verifyOtp({
+  //     phone,
+  //     token: '368908',
+  //     type: 'sms',
+  // })
 
+  const accessToken = process.env.SUPABASE_USER_ACCESS_TOKEN
+  const refreshToken = process.env.SUPABASE_USER_REFRESH_TOKEN
 
-const { data, error } = await supabase
-  .from('topics')
-  .select()
+  const sessionResponse = await supabase.auth.setSession({
+    access_token: accessToken,
+    refresh_token: refreshToken
+  })
 
-debugger
+  const queryResponse = await supabase
+    .from('instances')
+    .select()
+
+  debugger
+}
+
+run();
