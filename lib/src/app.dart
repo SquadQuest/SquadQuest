2c680908ce86c1b3eb85a5773079a58e95215865
Chris Alfano <chris@jarv.us>
2024-06-09 12:57:38 -0400

    refactor: move login/verify into screens tree

--- lib/src/app.dart
+++ lib/src/app.dart
@@ -2,11 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 
+import 'package:squad_quest/screens/login.dart';
+
 import 'sample_feature/sample_item_details_view.dart';
 // import 'sample_feature/sample_item_list_view.dart';
 import 'settings/settings_controller.dart';
 import 'settings/settings_view.dart';
-import 'login/login.dart';
 
 /// The Widget that configures your application.
 class MyApp extends StatelessWidget {
