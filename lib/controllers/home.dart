beb844bb066f69e548516376050333c3f67223c7
Chris Alfano <chris@jarv.us>
2024-06-09 22:17:48 -0400

    feat: initial event loading

---
+++ lib/controllers/home.dart
@@ -0,0 +1,36 @@
+// import 'dart:async';
+// import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'package:squad_quest/services/supabase.dart';
+
+final instancesListProvider =
+    AsyncNotifierProvider<InstancesList, List<Map>>(InstancesList.new);
+
+class InstancesList extends AsyncNotifier<List<Map>> {
+  @override
+  Future<List<Map>> build() async {
+    return fetch();
+  }
+
+  Future<List<Map>> fetch() async {
+    final supabase = ref.read(supabaseProvider);
+
+    final instances = await supabase
+        .from('instances')
+        .select()
+        .order('start_time_min', ascending: true);
+
+    return instances;
+  }
+
+  Future<void> refresh() async {
+    state = const AsyncLoading();
+    state = await AsyncValue.guard(fetch);
+  }
+}
+
+class HomeController extends AsyncNotifier<void> {
+  @override
+  void build() {}
+}
--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
 
+import 'package:squad_quest/controllers/home.dart';
 import 'package:squad_quest/controllers/auth.dart';
 
 class HomeScreen extends ConsumerStatefulWidget {
@@ -15,6 +16,7 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
   @override
   Widget build(BuildContext context) {
     final user = ref.watch(userProvider);
+    final instancesList = ref.watch(instancesListProvider);
 
     return SafeArea(
       child: Scaffold(
@@ -34,7 +36,33 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
           child: Column(
             children: [
               Text(
-                  'Logged in as ${user?.userMetadata!['first_name']} ${user?.userMetadata!['last_name']}')
+                  'Logged in as ${user?.userMetadata!['first_name']} ${user?.userMetadata!['last_name']}'),
+              Expanded(
+                child: instancesList.when(
+                    data: (instances) {
+                      return ListView.builder(
+                        itemCount: instances.length,
+                        itemBuilder: (context, index) {
+                          final Map item = instances[index];
+                          return ListTile(
+                            title: Text(
+                                '${item['title']}\nStarting between: ${item['start_time_min']}\nand: ${item['start_time_max']}'),
+                          );
+                        },
+                      );
+                    },
+                    loading: () =>
+                        const Center(child: CircularProgressIndicator()),
+                    error: (error, stackTrace) =>
+                        Center(child: Text('Error: $error'))),
+              ),
+              Center(
+                  child: ElevatedButton(
+                child: const Text('Refresh'),
+                onPressed: () {
+                  ref.read(instancesListProvider.notifier).refresh();
+                },
+              ))
             ],
           ),
         ),
