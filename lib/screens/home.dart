e958712320e6a766156bdcee1718c573028c5ed9
Chris Alfano <chris@jarv.us>
2024-06-11 21:32:02 -0400

    feat: add grouping to events list

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,10 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:go_router/go_router.dart';
+import 'package:grouped_list/grouped_list.dart';
 
 import 'package:squad_quest/drawer.dart';
 import 'package:squad_quest/controllers/instances.dart';
 import 'package:squad_quest/components/tiles/instance.dart';
+import 'package:squad_quest/models/instance.dart';
+
+enum _InstanceGroup {
+  current,
+  upcoming,
+  past,
+}
 
 class HomeScreen extends ConsumerStatefulWidget {
   const HomeScreen({super.key});
@@ -38,10 +46,46 @@ class _HomeScreenState extends ConsumerState<HomeScreen> {
           },
           child: instancesList.when(
               data: (instances) {
-                return ListView.builder(
-                  itemCount: instances.length,
-                  itemBuilder: (context, index) {
-                    return InstanceTile(instance: instances[index]);
+                final now = DateTime.now();
+
+                return GroupedListView(
+                  elements: instances,
+                  useStickyGroupSeparators: true,
+                  // floatingHeader: true,
+                  stickyHeaderBackgroundColor:
+                      Theme.of(context).scaffoldBackgroundColor,
+                  groupBy: (Instance instance) =>
+                      instance.startTimeMax.isBefore(now)
+                          ? _InstanceGroup.past
+                          : _InstanceGroup.upcoming,
+                  groupComparator: (group1, group2) {
+                    return group1.index.compareTo(group2.index);
+                  },
+                  groupSeparatorBuilder: (_InstanceGroup group) => Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Text(
+                        switch (group) {
+                          _InstanceGroup.past => 'In the past',
+                          _InstanceGroup.current => 'Happening now',
+                          _InstanceGroup.upcoming => 'Coming up',
+                        },
+                        textAlign: TextAlign.center,
+                        style: const TextStyle(fontSize: 18),
+                      )),
+                  itemBuilder: (context, element) {
+                    return InstanceTile(instance: element);
+                  },
+                  itemComparator: (instance1, instance2) {
+                    // sort past events in reverse chronological order
+                    if (instance1.startTimeMax.isBefore(now) &&
+                        instance2.startTimeMax.isBefore(now)) {
+                      return instance2.startTimeMax
+                          .compareTo(instance1.startTimeMax);
+                    }
+
+                    // sort current/upcoming events in chronological order
+                    return instance1.startTimeMax
+                        .compareTo(instance2.startTimeMax);
                   },
                 );
               },
