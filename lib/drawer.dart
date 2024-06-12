22686d20d7a751042a3735539873265ec7c6ff66
Chris Alfano <chris@jarv.us>
2024-06-11 23:22:09 -0400

    refactor: drive navigation from data structure

--- lib/drawer.dart
+++ lib/drawer.dart
@@ -8,6 +8,52 @@ final _appDrawerSelectionProvider = StateProvider<int>((ref) {
   return 0;
 });
 
+class _MenuItem {
+  static const divider = Key('divider');
+
+  final IconData icon;
+  final String label;
+  final String route;
+  final Future<void> Function(WidgetRef ref)? beforeNavigate;
+
+  _MenuItem({
+    required this.icon,
+    required this.label,
+    required this.route,
+    this.beforeNavigate,
+  });
+}
+
+final _menu = [
+  _MenuItem(
+    icon: Icons.home,
+    label: 'Home',
+    route: '/',
+  ),
+  _MenuItem(
+    icon: Icons.person,
+    label: 'Profile',
+    route: '/profile',
+  ),
+  _MenuItem(
+    icon: Icons.settings,
+    label: 'Settings',
+    route: '/settings',
+  ),
+  _MenuItem.divider,
+  _MenuItem(
+    icon: Icons.logout,
+    label: 'Sign out',
+    route: '/login',
+    beforeNavigate: (ref) async {
+      await ref.read(authControllerProvider.notifier).signOut();
+      ref.read(_appDrawerSelectionProvider.notifier).state = 0;
+    },
+  ),
+];
+
+final _menuItems = _menu.whereType<_MenuItem>().toList();
+
 class AppDrawer extends ConsumerStatefulWidget {
   const AppDrawer({super.key});
 
@@ -16,6 +62,20 @@ class AppDrawer extends ConsumerStatefulWidget {
 }
 
 class _AppDrawerState extends ConsumerState<AppDrawer> {
+  _onDestinationSelected(int newSelection) async {
+    final menuItem = _menuItems[newSelection];
+
+    ref.read(_appDrawerSelectionProvider.notifier).state = newSelection;
+
+    if (menuItem.beforeNavigate != null) {
+      await menuItem.beforeNavigate!(ref);
+    }
+
+    if (context.mounted) {
+      context.go(menuItem.route);
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     final user = ref.watch(userProvider);
@@ -23,28 +83,7 @@ class _AppDrawerState extends ConsumerState<AppDrawer> {
 
     return NavigationDrawer(
       selectedIndex: appDrawerSelection,
-      onDestinationSelected: (newSelection) async {
-        switch (newSelection) {
-          case 0:
-            context.go('/');
-            break;
-          case 1:
-            context.go('/profile');
-            break;
-          case 2:
-            context.go('/settings');
-            break;
-          case 3:
-            newSelection = 0;
-            await ref.read(authControllerProvider.notifier).signOut();
-            if (context.mounted) {
-              context.go('/login');
-            }
-            break;
-        }
-
-        ref.read(_appDrawerSelectionProvider.notifier).state = newSelection;
-      },
+      onDestinationSelected: _onDestinationSelected,
       children: <Widget>[
         UserAccountsDrawerHeader(
             decoration: const BoxDecoration(
@@ -53,26 +92,14 @@ class _AppDrawerState extends ConsumerState<AppDrawer> {
             accountName: Text(
                 '${user?.userMetadata!['first_name']} ${user?.userMetadata!['last_name']}'),
             accountEmail: Text('${user?.phone}')),
-        const NavigationDrawerDestination(
-          icon: Icon(Icons.home),
-          selectedIcon: Icon(Icons.home_filled),
-          label: Text('Home'),
-        ),
-        const NavigationDrawerDestination(
-          icon: Icon(Icons.person),
-          label: Text('Profile'),
-        ),
-        const NavigationDrawerDestination(
-          icon: Icon(Icons.settings),
-          label: Text('Settings'),
-        ),
-        const Divider(
-          thickness: 1,
-        ),
-        const NavigationDrawerDestination(
-          icon: Icon(Icons.logout),
-          label: Text('Sign out'),
-        ),
+        ..._menu.map((menuItem) => switch (menuItem) {
+              _MenuItem.divider => const Divider(thickness: 1),
+              (_MenuItem _) => NavigationDrawerDestination(
+                  icon: Icon(menuItem.icon),
+                  label: Text(menuItem.label),
+                ),
+              _ => throw 'Invalid menu item: $menuItem',
+            })
       ],
     );
   }
