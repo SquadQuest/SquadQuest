5e2950d7315f1a42314d99a58a74f5456f2b08ce
Chris Alfano <chris@jarv.us>
2024-06-12 20:32:33 -0400

    feat: add service role policies on friends

--- supabase-schema/policies/friends.sql
+++ supabase-schema/policies/friends.sql
@@ -7,3 +7,32 @@ to authenticated
 using (
     auth.uid() in (requester, requestee)
 );
+
+
+create policy "Service Role can read all friends"
+on "public"."friends"
+as PERMISSIVE
+for SELECT
+to service_role
+using (
+    true
+);
+
+create policy "Service Role can insert friends"
+on "public"."friends"
+as PERMISSIVE
+for INSERT
+to public
+with check (
+    true
+);
+
+
+create policy "Service Role can update friends"
+on "public"."friends"
+as PERMISSIVE
+for UPDATE
+to public
+with check (
+    true
+);
