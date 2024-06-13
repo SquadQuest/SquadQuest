0613d53061a51b764efdbe3bbc254b34c44f8c9e
Chris Alfano <chris@jarv.us>
2024-06-13 14:31:57 -0400

    feat: add debugResponse helper to functions http lib

--- supabase/functions/_shared/http.ts
+++ supabase/functions/_shared/http.ts
@@ -1,75 +1,86 @@
 function assertMethod(request: Request, method: string) {
-    if (request.method != method) {
-      throw new HttpError(`Only ${method} supported`, 405);
-    }
+  if (request.method != method) {
+    throw new HttpError(`Only ${method} supported`, 405);
+  }
+}
+
+function assertPost(request: Request) {
+  assertMethod(request, "POST");
+}
+
+const jsonHeaderRegexp = /^application\/json(;.*)?/;
+function assertJson(request: Request) {
+  if (!jsonHeaderRegexp.test(request.headers.get("content-type")!)) {
+    throw new HttpError(
+      "Request body must be JSON. Check Content-Type header.",
+      415,
+    );
+  }
+}
+
+async function getRequiredJsonParameters(
+  request: Request,
+  parameters: Array<string>,
+) {
+  assertJson(request);
+
+  let body;
+
+  try {
+    body = await request.json();
+  } catch (error) {
+    throw new HttpError(
+      `Failed to parse JSON request body: ${error?.message ?? error}`,
+      400,
+    );
   }
 
-  function assertPost(request: Request) {
-    assertMethod(request, "POST");
-  }
-
-  const jsonHeaderRegexp = /^application\/json(;.*)?/;
-  function assertJson(request: Request) {
-    if (!jsonHeaderRegexp.test(request.headers.get("content-type")!)) {
+  for (const parameter of parameters) {
+    if (!(parameter in body)) {
       throw new HttpError(
-        "Request body must be JSON. Check Content-Type header.",
-        415,
-      );
-    }
-  }
-
-  async function getRequiredJsonParameters(
-    request: Request,
-    parameters: Array<string>,
-  ) {
-    assertJson(request);
-
-    let body;
-
-    try {
-      body = await request.json();
-    } catch (error) {
-      throw new HttpError(
-        `Failed to parse JSON request body: ${error?.message ?? error}`,
+        `Required parameter missing from JSON request body: ${parameter}`,
         400,
       );
     }
-
-    for (const parameter of parameters) {
-      if (!(parameter in body)) {
-        throw new HttpError(
-          `Required parameter missing from JSON request body: ${parameter}`,
-          400,
-        );
-      }
-    }
-
-    return body;
   }
 
-  class HttpError extends Error {
-    code: number;
-    errorId?: string;
+  return body;
+}
 
-    constructor(message: string, code: number, errorId?: string) {
-      super(message);
+class HttpError extends Error {
+  code: number;
+  errorId?: string;
 
-      this.code = code;
-      this.errorId = errorId;
+  constructor(message: string, code: number, errorId?: string) {
+    super(message);
 
-      // Set the prototype explicitly.
-      Object.setPrototypeOf(this, HttpError.prototype);
-    }
+    this.code = code;
+    this.errorId = errorId;
 
-    sayHello() {
-      return "hello " + this.message;
-    }
+    // Set the prototype explicitly.
+    Object.setPrototypeOf(this, HttpError.prototype);
   }
 
-  export {
-    assertJson,
-    assertMethod,
-    assertPost,
-    getRequiredJsonParameters,
-    HttpError,
-  };
+  sayHello() {
+    return "hello " + this.message;
+  }
+}
+
+function debugResponse(message: string, data: object | undefined) {
+  return new Response(
+    JSON.stringify({ message, data }),
+    {
+      headers: { "Content-Type": "application/json" },
+      status: 501,
+    },
+  );
+}
+
+export {
+  assertJson,
+  assertMethod,
+  assertPost,
+  getRequiredJsonParameters,
+  HttpError,
+  debugResponse,
+};
