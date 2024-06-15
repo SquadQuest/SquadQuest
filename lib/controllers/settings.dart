ee9be4c6e28fbaf23d3a97ee38d4fd2143e65d04
Chris Alfano <chris@jarv.us>
2024-06-15 16:21:24 -0400

    feat: persist theme setting

--- lib/controllers/settings.dart
+++ lib/controllers/settings.dart
@@ -1,14 +1,38 @@
 // import 'dart:async';
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
-final themeModeProvider = StateProvider<ThemeMode>((ref) {
-  return ThemeMode.dark;
+final sharedPreferencesProvider = Provider<SharedPreferences>((ref) {
+  throw UnimplementedError();
 });
 
-class SettingsController extends Notifier<void> {
-  @override
-  void build() {
-    // no-op
+final themeModeProvider = StateProvider<ThemeMode>((ref) {
+  final prefs = ref.read(sharedPreferencesProvider);
+  final themeMode = prefs.getString('themeMode') ?? 'dark';
+  return ThemeMode.values.firstWhere((e) => e.name == themeMode);
+});
+
+final settingsControllerProvider = Provider<SettingsController>((ref) {
+  return SettingsController(ref);
+});
+
+class SettingsController {
+  final Ref ref;
+
+  SettingsController(this.ref) {
+    _init();
+  }
+
+  void _init() {
+    log('SettingsController._init');
+    final prefs = ref.read(sharedPreferencesProvider);
+
+    ref.listen(themeModeProvider, (_, themeMode) {
+      log('SettingsController.themeModeProvider=${themeMode.name}');
+      prefs.setString('themeMode', themeMode.name);
+    });
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
+import 'package:squadquest/controllers/settings.dart';
 import 'package:squadquest/services/supabase.dart';
 import 'package:squadquest/services/firebase.dart';
 import 'package:squadquest/app.dart';
@@ -12,6 +14,9 @@ void main() async {
   // Load .env file
   await dotenv.load(fileName: ".env");
 
+  // Initialize Shared Preferences
+  final sharedPreferences = await SharedPreferences.getInstance();
+
   // Initialize Supabase
   final supabaseApp = await buildSupabaseApp();
 
@@ -20,12 +25,14 @@ void main() async {
 
   // Initialize a custom provider container
   final container = ProviderContainer(overrides: [
+    sharedPreferencesProvider.overrideWithValue(sharedPreferences),
     supabaseAppProvider.overrideWithValue(supabaseApp),
-    firebaseAppProvider.overrideWithValue(firebaseApp)
+    firebaseAppProvider.overrideWithValue(firebaseApp),
   ]);
 
   // Initialize any providers that need to be always-on
   container.read(firebaseMessagingServiceProvider);
+  container.read(settingsControllerProvider);
 
   // Run the app
   runApp(UncontrolledProviderScope(
