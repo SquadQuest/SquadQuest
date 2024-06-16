60502e8a49de391221c088908e1d4c6d34a3a9bc
Chris Alfano <chris@jarv.us>
2024-06-16 14:27:30 -0400

    feat: push notifications set up for iOS

---
+++ ios/ImageNotification/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  ImageNotification
+//
+//  Created by Chris Alfano on 6/16/24.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
