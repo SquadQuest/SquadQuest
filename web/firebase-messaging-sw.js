449846240937c52bf65b101a9f8a922cc0e06fed
Chris Alfano <chris@jarv.us>
2024-06-16 20:25:26 -0400

    feat: web notifications working

--- web/firebase-messaging-sw.js
+++ web/firebase-messaging-sw.js
@@ -14,28 +14,62 @@ firebase.initializeApp({
 const messaging = firebase.messaging();
 
 messaging.onBackgroundMessage(function (payload) {
+  console.log('Received background message ', payload);
+
+  const payloadData = payload.data.json && JSON.parse(payload.data.json);
+  console.log('Parsed payload data ', payloadData);
+
   const promiseChain = clients
     .matchAll({
       type: "window",
       includeUncontrolled: true
     })
     .then(windowClients => {
-      for (let i = 0; i < windowClients.length; i++) {
-        const windowClient = windowClients[i];
-        windowClient.postMessage(payload);
+      for (client of windowClients) {
+        client.postMessage(payload);
       }
     })
     .then(() => {
       const title = payload.notification.title;
       const options = {
-        body: payload.notification.body
+        body: payload.notification.body,
+        icon: '/icons/Icon-192.png',
+        data: payloadData
       };
-      console.log(`Showing push notification: ${title}`, options);
+      console.log(`Showing notification: ${title}`, options);
       return registration.showNotification(title, options);
     });
   return promiseChain;
 });
 
 self.addEventListener('notificationclick', function (event) {
-  console.log('notification received: ', event)
+  console.log('notificationclick received: ', event);
+
+  const url = `https://cwqvpckp-80.use.devtunnels.ms/#/events/${event.notification.data.event.id}`;
+
+  event.notification.close(); // Android needs explicit close.
+  event.waitUntil(
+    clients.matchAll({ includeUncontrolled: true, type: 'window' }).then(windowClients => {
+      for (client of windowClients) {
+        // Check if there is already a window/tab open with the target URL
+        console.log(`looking for ${url} in ${client.url}`);
+        if (client.url === url && 'focus' in client) {
+          // If so, just focus it.
+          console.log('found! switching focus...');
+          return client.focus();
+        }
+      }
+
+      // If not, then open the target URL in a new window/tab.
+      if (clients.openWindow) {
+        return clients.openWindow(url);
+      }
+    })
+  );
 });
+
+// claim the current window immediately
+self.addEventListener('install', event => event.waitUntil(self.skipWaiting()));
+self.addEventListener('activate', event => event.waitUntil(self.clients.claim()));
+
+console.log('firebase-messaging-sw.js loaded!');
