e0937ff24ad43765636974495ff5f60ae8b2d85e
Chris Alfano <chris@jarv.us>
2024-06-17 21:20:37 -0400

    fix: handle secrets in Xcode Cloud

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -10,6 +10,10 @@ cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cl
 curl -fsSL https://fvm.app/install.sh | bash
 fvm use
 
+# Place secrets Xcode Cloud secret environment variables
+echo "${FLUTTER_ENV_BASE64}" | base64 -d >./.env
+echo "${GOOGLE_SERVICES_BASE64}" | base64 -d >./android/app/google-services.json
+
 # Install Flutter artifacts for iOS (--ios), or macOS (--macos) platforms.
 flutter precache --ios
 
