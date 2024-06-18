dd586d0a08d412ea1aa21c917fb122cf0a6f8cce
Chris Alfano <chris@jarv.us>
2024-06-17 21:00:51 -0400

    chore: use fvm to install flutter in Xcode cloud post-clone script

--- ios/ci_scripts/ci_post_clone.sh
+++ ios/ci_scripts/ci_post_clone.sh
@@ -6,9 +6,9 @@ set -e
 # The default execution directory of this script is the ci_scripts directory.
 cd $CI_PRIMARY_REPOSITORY_PATH # change working directory to the root of your cloned repo.
 
-# Install Flutter using git.
-git clone https://github.com/flutter/flutter.git --depth 1 -b stable $HOME/flutter
-export PATH="$PATH:$HOME/flutter/bin"
+# Install Flutter using fvm
+curl -fsSL https://fvm.app/install.sh | bash
+fvm use
 
 # Install Flutter artifacts for iOS (--ios), or macOS (--macos) platforms.
 flutter precache --ios
