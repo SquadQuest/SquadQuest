63e75e60434c7c94477b272e83e38a4e43b4c608
Chris Alfano <chris@jarv.us>
2024-06-20 17:31:56 -0400

    feat: render test line to map

--- lib/screens/map.dart
+++ lib/screens/map.dart
@@ -5,6 +5,7 @@ import 'package:maplibre_gl/maplibre_gl.dart';
 import 'package:squadquest/logger.dart';
 import 'package:squadquest/drawer.dart';
 import 'package:squadquest/services/location.dart';
+import 'package:squadquest/services/supabase.dart';
 
 class MapScreen extends ConsumerStatefulWidget {
   const MapScreen({super.key});
@@ -14,6 +15,57 @@ class MapScreen extends ConsumerStatefulWidget {
 }
 
 class _MapScreenState extends ConsumerState<MapScreen> {
+  MapLibreMapController? controller;
+
+  void _onMapCreated(MapLibreMapController controller) {
+    logger.d('MapScreen._onMapCreated');
+    this.controller = controller;
+  }
+
+  void _onStyleLoadedCallback() async {
+    logger.d('MapScreen._onStyleLoadedCallback');
+    // await controller!.addLine(
+    //   const LineOptions(
+    //     geometry: [LatLng(37.4220, -122.0841), LatLng(37.4240, -122.0941)],
+    //     lineColor: "#ff0000",
+    //     lineWidth: 14.0,
+    //     lineOpacity: 0.5,
+    //   ),
+    // );
+    _loadTracks();
+  }
+
+  void _loadTracks() async {
+    final supabase = ref.read(supabaseClientProvider);
+    final points = await supabase
+        .from('location_tracks')
+        .select('location_text')
+        .order('timestamp')
+        .withConverter((data) {
+      return data.map((row) {
+        final [longitude, latitude] = row['location_text']
+            .substring(6, row['location_text'].length - 1)
+            .split(' ');
+        return LatLng(double.parse(latitude), double.parse(longitude));
+      }).toList();
+    });
+
+    // for (final point in points) {
+    //   controller!.addCircle(CircleOptions(
+    //     geometry: point,
+    //     circleRadius: 5.0,
+    //     circleColor: '#ff0000',
+    //     circleOpacity: 0.5,
+    //   ));
+    // }
+    controller!.addLine(LineOptions(
+      geometry: points,
+      lineColor: '#ff0000',
+      lineWidth: 5.0,
+      lineOpacity: 0.5,
+    ));
+  }
+
   @override
   void initState() {
     super.initState();
@@ -32,6 +84,8 @@ class _MapScreenState extends ConsumerState<MapScreen> {
         ),
         drawer: const AppDrawer(),
         body: MapLibreMap(
+          onMapCreated: _onMapCreated,
+          onStyleLoadedCallback: _onStyleLoadedCallback,
           initialCameraPosition: const CameraPosition(
             target: LatLng(39.9550, -75.1605),
             zoom: 11.75,
