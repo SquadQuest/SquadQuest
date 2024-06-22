3d590c7bebeb8d76b04825ecdeaec7c354b78f91
Chris Alfano <chris@jarv.us>
2024-06-22 14:12:06 -0400

    feat: add event field to location points table

--- supabase/tables/location_points.sql
+++ supabase/tables/location_points.sql
@@ -2,11 +2,13 @@ create table public.location_points (
   id uuid not null default gen_random_uuid (),
   created_at timestamp with time zone not null default now(),
   created_by uuid null default uid (),
+  event uuid not null,
   timestamp timestamp with time zone not null,
-  location geography(POINT) not null,
-  location_text text GENERATED ALWAYS AS (ST_AsText(location)) STORED,
+  location geography not null,
+  location_text text null,
   constraint location_points_pkey primary key (id),
-  constraint public_location_points_created_by_fkey foreign key (created_by) references profiles (id) on delete cascade
+  constraint public_location_points_created_by_fkey foreign key (created_by) references profiles (id) on delete cascade,
+  constraint public_location_points_event_fkey foreign key (event) references instances (id) on update cascade
 );
 
 -- delete data older than 3 days every hour
