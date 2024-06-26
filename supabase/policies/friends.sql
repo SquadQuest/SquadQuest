9de0df6afdd38f1d95b6ce6e6300994785585af9
Chris Alfano <chris@jarv.us>
2024-06-26 08:28:59 -0400

    refactor: merge policies into table files and normalize

--- supabase/policies/friends.sql
+++
@@ -1,9 +0,0 @@
--- Authenticated users can read all their friends of any status
-create policy "Authenticated users can read all their friends of any status"
-on "public"."friends"
-as PERMISSIVE
-for SELECT
-to authenticated
-using (
-    auth.uid() in (requester, requestee)
-);
