03e69317b25189b0a4cc4c5eeb1a77126748c959
Chris Alfano <chris@jarv.us>
2024-07-04 17:58:14 +0000

    fix: reduce scrubbed profiles to anon data

--- supabase/functions/_shared/squadquest.ts
+++ supabase/functions/_shared/squadquest.ts
@@ -2,8 +2,6 @@ function scrubProfile(userProfile: { [key: string]: string | number }) {
   return {
     id: userProfile?.id,
     first_name: userProfile?.first_name,
-    last_name: userProfile?.last_name,
-    photo: userProfile?.photo,
   };
 }
 
