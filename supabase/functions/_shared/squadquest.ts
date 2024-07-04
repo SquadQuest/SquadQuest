a60cefb5ecc960c51ea83a0d3c5d7a3ac32e3a23
Chris Alfano <chris@jarv.us>
2024-07-04 21:51:18 +0000

    fix: restore full data to scrubProfile (for now)

--- supabase/functions/_shared/squadquest.ts
+++ supabase/functions/_shared/squadquest.ts
@@ -2,6 +2,8 @@ function scrubProfile(userProfile: { [key: string]: string | number }) {
   return {
     id: userProfile?.id,
     first_name: userProfile?.first_name,
+    last_name: userProfile?.last_name,
+    photo: userProfile?.photo,
   };
 }
 
