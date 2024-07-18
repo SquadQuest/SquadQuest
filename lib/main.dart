6d24f3794bf14e92ce4e516c9a0edf90c86e62aa
Chris Alfano <chris@jarv.us>
2024-07-18 16:23:09 -0400

    fixup! feat: add sentry

--- lib/main.dart
+++ lib/main.dart
@@ -75,6 +75,8 @@ void main() async {
       // The sampling rate for profiling is relative to tracesSampleRate
       // Setting to 1.0 will profile 100% of sampled transactions:
       options.profilesSampleRate = 1.0;
+
+      options.attachViewHierarchy = true;
     },
     appRunner: () => runApp(UncontrolledProviderScope(
       container: container,
