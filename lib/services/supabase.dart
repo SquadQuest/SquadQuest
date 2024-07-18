7d0dc19f46bc650f7e8a420db7a8a1c49b2d192b
Chris Alfano <chris@jarv.us>
2024-07-18 16:22:03 -0400

    feat: enrich sentry with user data

--- lib/services/supabase.dart
+++ lib/services/supabase.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sentry_flutter/sentry_flutter.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:http/http.dart' as http;
@@ -56,6 +57,17 @@ Future<Supabase> buildSupabaseApp() async {
         'session.user': data.session?.user
       }
     });
+
+    // integrate with Sentry
+    final sentryUser = data.session == null
+        ? null
+        : SentryUser(
+            id: data.session?.user.id,
+            name: data.session?.user.userMetadata?['first_name'],
+          );
+    Sentry.configureScope((scope) => scope.setUser(sentryUser));
+
+    // complete supabaseInitialized future when session is available
     if (data.event == AuthChangeEvent.initialSession) {
       logger.t('supabase initialized');
       _supabaseInitializedCompleter.complete();
