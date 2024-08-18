ed4253743af217ac4810de4e7d00e81683e851d4
Chris Alfano <chris@jarv.us>
2024-08-18 15:02:48 -0400

    chore: remove unused imports

--- lib/screens/event_details.dart
+++ lib/screens/event_details.dart
@@ -3,7 +3,6 @@ import 'dart:io';
 import 'dart:math';
 
 import 'package:flutter/foundation.dart';
-import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
