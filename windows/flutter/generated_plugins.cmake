bc346ce5336db2391a78ca313b6d997f98a80f15
Chris Alfano <chris@jarv.us>
2024-09-10 20:24:10 -0400

    chore(deps): flutter pub upgrade

--- windows/flutter/generated_plugins.cmake
+++ windows/flutter/generated_plugins.cmake
@@ -7,11 +7,11 @@ list(APPEND FLUTTER_PLUGIN_LIST
   file_selector_windows
   firebase_core
   flutter_timezone
-  sentry_flutter
   url_launcher_windows
 )
 
 list(APPEND FLUTTER_FFI_PLUGIN_LIST
+  sentry_flutter
 )
 
 set(PLUGIN_BUNDLED_LIBRARIES)
