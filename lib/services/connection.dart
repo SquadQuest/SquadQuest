bf35e118691046a4f8efdb7ff5d9fb3792a89dab
Chris Alfano <chris@jarv.us>
2024-09-21 20:37:34 -0400

    fix: detect profile load failure and display error

---
+++ lib/services/connection.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+import 'package:squadquest/router.dart';
+
+class ConnectionService {
+  static Future<void> showConnectionErrorDialog() {
+    return showDialog<void>(
+      context: navigatorKey.currentContext!,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('Unable to connect'),
+          content: const Text(
+              'SquadQuest is unable to connect to the server. Please check your internet connection and try again.'),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text('OK'),
+            ),
+          ],
+        );
+      },
+    );
+  }
+}
