a813b432b8cf0deb670d2edb0b7598ae8bc76ebd
Chris Alfano <chris@jarv.us>
2024-12-24 17:43:49 -0500

    feat: hide non-friend invites at the policy level

--- supabase/policies/instance_members.sql
+++ supabase/policies/instance_members.sql
@@ -1,3 +1,18 @@
-create policy "Authenticated users can read all xrefs" on "public"."instance_members" as PERMISSIVE for
+create policy "Authenticated users can read own, friends, an non-invite RSVPs" on "public"."instance_members" as PERMISSIVE for
 SELECT
-  to authenticated using (true);
+  to authenticated using (
+    member = auth.uid ()
+    OR created_by = auth.uid()
+    OR status != 'invited'
+    OR EXISTS (
+      SELECT
+      FROM
+        friends
+      WHERE
+        friends.status = 'accepted'
+        AND (
+          auth.uid () in (requester, requestee)
+          AND member in (requester, requestee)
+        )
+    )
+  );
