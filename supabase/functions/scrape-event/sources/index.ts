45afb12b18dc99de48ba40600a21a87b735dd186
Chris Alfano <chris@jarv.us>
2024-12-27 17:13:47 -0500

    feat(api): add Partiful scraper

--- supabase/functions/scrape-event/sources/index.ts
+++ supabase/functions/scrape-event/sources/index.ts
@@ -1,5 +1,6 @@
 import eventbrite from "./eventbrite.ts";
 import facebook from "./facebook.ts";
 import residentAdvisor from "./resident-advisor.ts";
+import partiful from "./partiful.ts";
 
-export default { eventbrite, facebook, residentAdvisor };
+export default { eventbrite, facebook, residentAdvisor, partiful };
